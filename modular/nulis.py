ef8e64e5515b7983e1832802129265f53eca863b
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-17 09:47:09 +0700

    update modular/nulis.py

--- modular/nulis.py
+++ modular/nulis.py
@@ -73,7 +73,7 @@ def write_on_image(text, filename="output.jpg", line_spacing=50, enter_spacing=1
     template = Image.open("Mix/core/bahan.jpg")
     draw = ImageDraw.Draw(template)
     font = ImageFont.truetype("Mix/core/font.ttf", 32)
-    x, y = 125, 230
+    x, y = 280, 350
     paragraphs = text.split("\n")
 
     for paragraph in paragraphs:
