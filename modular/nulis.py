ecb2dfc3f708f21895a227a9aa211d11bb0477b1
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-17 09:34:31 +0700

    update modular/nulis.py

--- modular/nulis.py
+++ modular/nulis.py
@@ -63,30 +63,32 @@ async def _(c, m):
 
 
 import os
-
 from PIL import Image, ImageDraw, ImageFont
-
 from Mix.core import *
 
-
-def write_on_image(text, filename="output.jpg"):
+def write_on_image(text, filename="output.jpg", line_spacing=50, enter_spacing=150):
     template = Image.open("Mix/core/bahan.jpg")
     draw = ImageDraw.Draw(template)
     font = ImageFont.truetype("Mix/core/font.ttf", 32)
     x, y = 125, 240
-    lines = []
-    words = text.split(" ")
-    line = []
-    for word in words:
-        line.append(word)
-        w, h = draw.textsize(" ".join(line), font=font)
-        if w > template.width - 200:
-            lines.append(" ".join(line[:-1]))
-            line = [word]
-    lines.append(" ".join(line))
-    for line in lines:
-        draw.text((x, y), line, font=font, fill="black")
-        y += h + 100
+    paragraphs = text.split('\n')
+
+    for paragraph in paragraphs:
+        lines = []
+        words = paragraph.split(" ")
+        line = []
+        for word in words:
+            line.append(word)
+            w, h = draw.textsize(" ".join(line), font=font)
+            if w > template.width - 200:
+                lines.append(" ".join(line[:-1]))
+                line = [word]
+        lines.append(" ".join(line))
+
+        for line in lines:
+            draw.text((x, y), line, font=font, fill="black")
+            y += h + line_spacing
+        y += enter_spacing - line_spacing
     template.save(filename)
 
 
@@ -100,7 +102,9 @@ async def _(c: nlx, m):
     if not text:
         await m.reply("Silakan balas pesan atau masukkan teks setelah perintah.")
         return
+    
     output_filename = "output.jpg"
     write_on_image(text, output_filename)
     await m.reply_photo(photo=output_filename)
     os.remove(output_filename)
+
