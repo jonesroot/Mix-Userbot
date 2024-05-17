a5dce4076bd547202e9b909bfc2464eabd733083
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-17 10:17:31 +0700

    update modular/nulis.py

--- modular/nulis.py
+++ modular/nulis.py
@@ -70,9 +70,9 @@ def write_on_image(text, filename="output.jpg", line_spacing=50, enter_spacing=9
     template = Image.open("Mix/core/bahan.jpg")
     draw = ImageDraw.Draw(template)
     font = ImageFont.truetype("Mix/core/font.ttf", 32)
-    margin_left = 125
-    margin_right = 125
-    margin_top = 240
+    margin_left = 230
+    margin_right = 230
+    margin_top = 350
     margin_bottom = 100
 
     x = margin_left
