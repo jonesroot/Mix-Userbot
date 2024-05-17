84338357a5dac609f2c0590c3721a9f5e9351205
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-18 02:54:07 +0700

    update modular/rmbg.py

--- modular/rmbg.py
+++ modular/rmbg.py
@@ -8,11 +8,10 @@
 
 
 import os
-
 import requests
 from removebg import RemoveBg
-
 from Mix import *
+
 
 __modles__ = "RemoveBg"
 __help__ = """
@@ -23,17 +22,25 @@ Removal Background
 
 
 async def rem_bg(image_path):
-    rmbg = RemoveBg("KxZHg1ZjxsiU5TLca4kjWptR", "error.log")
-    hasil = rmbg.remove_background_from_img_file(image_path)
-    return hasil
+    try:
+        rmbg = RemoveBg("KxZHg1ZjxsiU5TLca4kjWptR", "error.log")
+        hasil = rmbg.remove_background_from_img_file(image_path)
+        return hasil
+    except Exception as e:
+        print("Error:", str(e))
+        return None
 
 
 async def rbg_link(link):
-    rmbg = RemoveBg("YOUR-API-KEY", "error.log")
-    hasil = rmbg.remove_background_from_img_url(link)
-    return hasil
-
-
+    try:
+        rmbg = RemoveBg("KxZHg1ZjxsiU5TLca4kjWptR", "error.log")
+        hasil = rmbg.remove_background_from_img_url(link)
+        return hasil
+    except Exception as e:
+        print("Error:", str(e))
+        return None
+  
+  
 @ky.ubot("rmbg|rbg", sudo=True)
 async def _(c: nlx, m):
     em = Emojik()
@@ -41,10 +48,8 @@ async def _(c: nlx, m):
     rep = m.reply_to_message
     pros = await m.reply(cgr("proses").format(em.proses))
 
-    if rep and (
-        rep.photo or rep.document and rep.document.mime_type.startswith("image/")
-    ):
-        photo = rep.photo.file_id if rep.photo else rep.document.file_id
+    if rep:
+        photo = rep.photo
         photo_file_path = await c.download_media(photo)
 
         try:
@@ -52,11 +57,6 @@ async def _(c: nlx, m):
             if hasil:
                 await m.reply_document(hasil, reply_to_message_id=ReplyCheck(m))
                 os.remove(hasil)
-            else:
-                await m.reply_text(
-                    "Maaf, terjadi kesalahan dalam menghapus latar belakang gambar.",
-                    reply_to_message_id=ReplyCheck(m),
-                )
         except Exception as e:
             await m.reply_text(
                 f"Terjadi kesalahan: {str(e)}", reply_to_message_id=ReplyCheck(m)
@@ -69,7 +69,6 @@ async def _(c: nlx, m):
         if len(args) > 1:
             image_url = args[1]
             output_image_path = "input_image.jpg"
-
             try:
                 response = requests.get(image_url)
                 if response.status_code == 200:
