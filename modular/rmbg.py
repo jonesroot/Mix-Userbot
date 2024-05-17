eef77dfc31bfa35b627ea2d401e17ffcc1e0ab32
jonesroot <jonesroot@users.noreply.github.com>
2024-05-17 18:59:02 +0000

    Auto Fix

--- modular/rmbg.py
+++ modular/rmbg.py
@@ -6,11 +6,9 @@
 """
 ################################################################
 
-import io
 import os
 
 import requests
-from PIL import Image
 
 from Mix import *
 
@@ -23,25 +21,23 @@ Removal Background
 
 
 async def rem_bg(image_path):
-    url = 'https://api.removal.ai/3.0/remove'
+    url = "https://api.removal.ai/3.0/remove"
     headers = {
-        'accept': 'application/json',
-        'Rm-Token': '45FA9185-021A-E498-FF97-1A8DB4C76666',
+        "accept": "application/json",
+        "Rm-Token": "45FA9185-021A-E498-FF97-1A8DB4C76666",
     }
-    files = {
-        'image_file': (image_path, open(image_path, 'rb'), 'image/jpeg')
-    }
+    files = {"image_file": (image_path, open(image_path, "rb"), "image/jpeg")}
 
     response = requests.post(url, headers=headers, files=files)
 
     if response.status_code == 200:
         data = response.json()
-        high_res_url = data.get('high_resolution')
-        
+        high_res_url = data.get("high_resolution")
+
         if high_res_url:
             high_res_image = requests.get(high_res_url)
             output_image_path = "rmbg.png"
-            with open(output_image_path, 'wb') as f:
+            with open(output_image_path, "wb") as f:
                 f.write(high_res_image.content)
             return output_image_path
         else:
@@ -57,7 +53,9 @@ async def _(c: nlx, m):
     rep = m.reply_to_message
     pros = await m.reply(cgr("proses").format(em.proses))
 
-    if rep and (rep.photo or rep.document and rep.document.mime_type.startswith("image/")):
+    if rep and (
+        rep.photo or rep.document and rep.document.mime_type.startswith("image/")
+    ):
         photo = rep.photo.file_id if rep.photo else rep.document.file_id
         photo_file_path = await c.download_media(photo)
 
@@ -83,11 +81,11 @@ async def _(c: nlx, m):
         if len(args) > 1:
             image_url = args[1]
             output_image_path = "input_image.jpg"
-            
+
             try:
                 response = requests.get(image_url)
                 if response.status_code == 200:
-                    with open(output_image_path, 'wb') as f:
+                    with open(output_image_path, "wb") as f:
                         f.write(response.content)
                     hasil = await rem_bg(output_image_path)
                     if hasil:
@@ -111,6 +109,9 @@ async def _(c: nlx, m):
                 if os.path.exists(output_image_path):
                     os.remove(output_image_path)
         else:
-            await m.reply_text("Mohon balas ke gambar atau masukkan URL gambar.", reply_to_message_id=ReplyCheck(m))
+            await m.reply_text(
+                "Mohon balas ke gambar atau masukkan URL gambar.",
+                reply_to_message_id=ReplyCheck(m),
+            )
 
     await pros.delete()
