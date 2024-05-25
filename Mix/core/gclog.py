021eddcd638e158166c76845727b9f9919aadc64
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-26 03:36:26 +0700

    update Mix/core/gclog.py

--- Mix/core/gclog.py
+++ Mix/core/gclog.py
@@ -93,9 +93,15 @@ async def getFinish():
                     can_manage_video_chats=True,
                 ),
             )
-            await bot.send_message(
+        except:
+            ndB.del_key("TAG_LOG")
+            execvp(executable, [executable, "-m", "Mix"])
+
+
+"""
+await bot.send_message(
                 TAG_LOG,
-                f"""
+                f
 <b>Userbot Successfully Deploy !!</b>
 
 <b>Modules : {len(CMD_HELP)}</b>
@@ -103,8 +109,6 @@ async def getFinish():
 <b>Pyrogram : {pyrover}</b>
 <b>Pytgcalls : {pytgver}</b>
 <b>Prefixes : {xx}</b>
-""",
+,
             )
-        except:
-            ndB.del_key("TAG_LOG")
-            execvp(executable, [executable, "-m", "Mix"])
+"""
