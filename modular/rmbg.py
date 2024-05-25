984cd0ea9ddea61d6bd704143ac772db8198f0fe
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-25 12:44:45 +0700

    update modular/rmbg.py

--- modular/rmbg.py
+++ modular/rmbg.py
@@ -25,41 +25,50 @@ Removal Background
 async def rem_bg(image_path):
     try:
         rmbg = RemoveBg("KxZHg1ZjxsiU5TLca4kjWptR", "error.log")
+        output_path = "no-bg.png"
         rmbg.remove_background_from_img_file(image_path)
-        hasil = "no-bg.png"
-        return hasil
+        if os.path.exists(output_path):
+            return output_path
+        else:
+            raise FileNotFoundError("Output file not found")
     except Exception as e:
         print("Error:", str(e))
         return None
-
 
 async def rbg_link(link):
     try:
         rmbg = RemoveBg("KxZHg1ZjxsiU5TLca4kjWptR", "error.log")
+        output_path = "no-bg.png"
         rmbg.remove_background_from_img_url(link)
-        hasil = "no-bg.png"
-        return hasil
+        if os.path.exists(output_path):
+            return output_path
+        else:
+            raise FileNotFoundError("Output file not found")
     except Exception as e:
         print("Error:", str(e))
         return None
 
-
 @ky.ubot("rmbg|rbg", sudo=True)
-async def _(c: nlx, m):
+async def _(c: Client, m):
     em = Emojik()
     em.initialize()
     rep = m.reply_to_message
     pros = await m.reply(cgr("proses").format(em.proses))
 
-    if rep:
+    if rep and rep.photo:
         photo = rep.photo
         photo_file_path = await c.download_media(photo.file_id)
-
+        
         try:
             hasil = await rem_bg(photo_file_path)
             if hasil:
                 await m.reply_document(hasil, reply_to_message_id=ReplyCheck(m))
                 os.remove(hasil)
+            else:
+                await m.reply_text(
+                    "Maaf, terjadi kesalahan dalam menghapus latar belakang gambar.",
+                    reply_to_message_id=ReplyCheck(m),
+                )
         except Exception as e:
             await m.reply_text(
                 f"Terjadi kesalahan: {str(e)}", reply_to_message_id=ReplyCheck(m)
