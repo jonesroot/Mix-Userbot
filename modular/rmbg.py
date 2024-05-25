7f1626e26c9049c4188d90ba3fa26a48e220209e
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-25 12:30:19 +0700

    update modular/rmbg.py

--- modular/rmbg.py
+++ modular/rmbg.py
@@ -26,7 +26,7 @@ async def rem_bg(image_path):
     try:
         rmbg = RemoveBg("KxZHg1ZjxsiU5TLca4kjWptR", "error.log")
         rmbg.remove_background_from_img_file(image_path)
-        hasil = "no-bg.png"
+        hasil = "*no-bg.png"
         return hasil
     except Exception as e:
         print("Error:", str(e))
@@ -37,7 +37,7 @@ async def rbg_link(link):
     try:
         rmbg = RemoveBg("KxZHg1ZjxsiU5TLca4kjWptR", "error.log")
         rmbg.remove_background_from_img_url(link)
-        hasil = "no-bg.png"
+        hasil = "*no-bg.png"
         return hasil
     except Exception as e:
         print("Error:", str(e))
